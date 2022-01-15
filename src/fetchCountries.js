e9c9e2a1614b420dbed202454cf03520ad0f9e93
Stanislav Kostiuchenko <kostiusta@gmail.com>
2022-01-15 16:53:34 +0200

    Added errors handler for fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,6 +3,10 @@ const QUERY_PARAMS = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
   return fetch(`${DEFAULE_URL}${name}?${QUERY_PARAMS}`).then(response => {
-    return response.json();
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(response.statusText);
   });
 }
